28b4384c0d3479a9355f96462092d59dd85afb60
Akane277353 <39272953+Akane277353@users.noreply.github.com>
2023-03-12 11:38:51 +0100

    idk

--- launcher/app/src/main/java/com/example/jetpack_launcher/MainActivity.kt
+++ launcher/app/src/main/java/com/example/jetpack_launcher/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.jetpack_launcher
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Surface
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_launcher.ui.theme.JetpacklauncherTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,8 +25,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 androidx.compose.material3.Surface(
                     modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    View()
                 }
             }
         }
@@ -28,14 +35,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    androidx.compose.material3.Text(text = "Hello $name!")
+fun View() {
+    var txt by remember { mutableStateOf("oui") }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        androidx.compose.material3.Button(onClick = { txt = "non" }) {
+            androidx.compose.material3.Text(txt)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpacklauncherTheme {
-        Greeting("Android")
+        View()
     }
 }
